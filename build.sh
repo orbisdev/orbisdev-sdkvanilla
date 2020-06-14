357634468db230a1bbad796dd88c8e43ac5a9594
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-06-14 12:07:10 +0200

    Compile libgen

---
+++ build.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+set -e
+
+sh stage0-libgen.sh
---
+++ stage0-libgen.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+set -e
+
+wget https://github.com/orbisdev/orbis-libs-gen/releases/latest/download/lib_s.tar.gz
+
+rm -rf lib_s
+tar -zxvf lib_s.tar.gz
+cd lib_s
+
+for i in *.S
+do
+    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}_stub.so $i
+done
+
+cp *.so $ORBISDEV/lib
