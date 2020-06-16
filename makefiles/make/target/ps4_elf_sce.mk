0646db9eb8f7a124e38fb0b7b2229988ed42b237
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2020-06-16 20:26:26 +0200

    WIP new structure

--- makefiles/make/target/ps4_elf_sce.mk
+++ makefiles/make/target/ps4_elf_sce.mk
@@ -2,7 +2,7 @@
 AssemblerFlags += -target x86_64-scei-ps4-elf -fPIE -dynamic
 CompilerFlags = -cc1 -triple x86_64-scei-ps4-elf -munwind-tables -D__PS4__ -emit-obj $(IncludePath) $(Debug)
 CompilerFlagsCpp = -cc1 -triple x86_64-scei-ps4-elf -munwind-tables -D__PS4__ -emit-obj $(IncludePath) $(Debug)
-LinkerFlags += -Wl,--gc-sections -Wl,-z -Wl,max-page-size=0x4000 -Wl,--dynamic-linker="/libexec/ld-elf.so.1" -Wl,-pie -Wl,--eh-frame-hdr -L$(PS4SDK)/lib -target x86_64-scei-ps4-elf -T $(PS4SDK)/linker.x
+LinkerFlags += -Wl,--gc-sections -Wl,-z -Wl,max-page-size=0x4000 -Wl,--dynamic-linker="/libexec/ld-elf.so.1" -Wl,-pie -Wl,--eh-frame-hdr -L$(ORBISDEV)/usr/lib -target x86_64-scei-ps4-elf -T $(ORBISDEV)/linker.x
 CrtFile ?= $(ORBISDEV)/crt0.s  
 #CrtFile ?= $(ORBISDEV)/crt0musl.s   $(ORBISDEV)/usr/lib/Scrt1.o $(ORBISDEV)/usr/lib/crti.o $(ORBISDEV)/usr/lib/crtn.o
 ###################################
