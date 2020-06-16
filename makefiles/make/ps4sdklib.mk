a6b94163fc2a0be4dc23cf87979e547cc51d7f9f
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2020-06-16 11:47:02 +0200

    new orbis-ar change

--- makefiles/make/ps4sdklib.mk
+++ makefiles/make/ps4sdklib.mk
@@ -4,7 +4,7 @@ Assembler ?= clang
 Compiler ?= clang -v
 CompilerCpp ?= clang++ -v
 Linker ?= clang -v
-Archiver ?= ar
+Archiver ?= orbis-ar
 ObjectCopy ?= objcopy
 
 ###################################
