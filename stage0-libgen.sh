f6f85e44bfa10cb6f04641513706d5b39761702e
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2021-08-25 18:23:08 +0200

    avoid generation problem with --shared option in modern clang
    Sony disabled gold linker support and now with --shared option emit -oformat=so incompatible with our orbis-ld from binutils
    Tryng to fix and do the whole thing in two step instead with clang versions after (https://github.com/llvm/llvm-project/commit/2956cc50f3405ae32c8c6d6e2c63fe8cb6456fa2#diff-7357d0bdfdcdc5f90213ef2a63f4487f2905cff6b0a3d3e55655d4afe143ec41)

--- stage0-libgen.sh
+++ stage0-libgen.sh
@@ -9,7 +9,12 @@ cd lib_s
 
 for i in *.S
 do
-    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}_stub.so $i --verbose
+    clang --target=x86_64-scei-ps4 -nostdlib -m64 -c -o  ${i%.S}_stub.o $i
+done
+
+for i in *.o
+do
+    orbis-ld --eh-frame-hdr -Bshareable --enable-new-dtags -o ${i%.o}_stub.so $i
 done
 
 mkdir -p $ORBISDEV/usr/lib
