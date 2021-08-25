db8507af77c05734d95678fc1156519288b0eb7b
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-08-25 16:27:34 +0200

    Add verbose output

--- stage0-libgen.sh
+++ stage0-libgen.sh
@@ -9,7 +9,7 @@ cd lib_s
 
 for i in *.S
 do
-    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}_stub.so $i
+    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}_stub.so $i --verbose
 done
 
 mkdir -p $ORBISDEV/usr/lib
