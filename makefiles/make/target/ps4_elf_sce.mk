abe8dab29a850ea48f20c359bb81f95ec958f90e
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-10-03 11:01:43 +0200

    Change linker flags

--- makefiles/make/target/ps4_elf_sce.mk
+++ makefiles/make/target/ps4_elf_sce.mk
@@ -2,7 +2,7 @@
 AssemblerFlags += --target=x86_64-scei-ps4 -fPIE -dynamic
 CompilerFlags += --target=x86_64-scei-ps4 -D__PS4__ -D__ORBIS__ $(IncludePath) $(Debug)
 CompilerFlagsCpp += --target=x86_64-scei-ps4 -D__PS4__ -D__ORBIS__ $(IncludePath) $(Debug)
-LinkerFlags += -Wl,--gc-sections -Wl,-z -Wl,max-page-size=0x4000 -Wl,--dynamic-linker="/libexec/ld-elf.so.1" -Wl,-pie -Wl,--eh-frame-hdr -L$(ORBISDEV)/usr/lib -target x86_64-scei-ps4-elf -T $(ORBISDEV)/linker.x
+LinkerFlags += --gc-sections -z max-page-size=0x4000 --dynamic-linker=/libexec/ld-elf.so.1 -pie --eh-frame-hdr -L$(ORBISDEV)/usr/lib -target x86_64-scei-ps4-elf -T $(ORBISDEV)/linker.x
 CrtFile ?= $(ORBISDEV)/crt0.o
 ###################################
 ###################################
