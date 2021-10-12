7b7e43a950745e3babb12269b4077a3e9e562b25
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2021-10-12 19:32:26 +0200

    avoid double include flags

--- makefiles/make/target/ps4_elf_sce.mk
+++ makefiles/make/target/ps4_elf_sce.mk
@@ -1,8 +1,8 @@
 ###################################
 PlatformFlags += --target=x86_64-scei-ps4 -D__PS4__ -D__ORBIS__
 AssemblerFlags += $(PlatformFlags) -fPIE -dynamic
-CompilerFlags +=  $(PlatformFlags) $(IncludePath) $(Debug)
-CompilerFlagsCpp += $(PlatformFlags) $(IncludePath) $(Debug)
+CompilerFlags +=  $(PlatformFlags) 
+CompilerFlagsCpp += $(PlatformFlags) 
 LinkerFlags += -T $(ORBISDEV)/usr/lib/linker.x --dynamic-linker=/libexec/ld-elf.so.1 --gc-sections -z max-page-size=0x4000  -pie --eh-frame-hdr
 CrtFile ?= $(ORBISDEV)/usr/lib/crt0.o
 ###################################
